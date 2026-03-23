48f04882997945551573a8ad98b8cc01351197da
jjose21-source <j.jose21@escola.pr.gov.br>
2026-03-23 09:08:20 -0300

    Primeira ativadade

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-// run `node index.js` in the terminal
+// Soma
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+let num1 = 2;
+let num2 = 4;
+
+let soma = num1 + num2;
+
+console.log("O resultado da soma é:",soma);
+
+// Subtração
+
+let subtracao = 50 - 25;
+
+console.log("O resultado da subtração é:",subtracao);
+
+// Mutiplicação
+
+let multiplicacao = 10 * 2;
+
+console.log("O resultado da multiplicação é:",multiplicacao);
+
+// Divisão
+
+let divisao = 100 / 4;
+
+console.log("O quociente da divisão é:",divisao)
+
