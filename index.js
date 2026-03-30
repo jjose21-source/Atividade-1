e7d37cb83dce94b6ec70a3a94b10e01bd1eda6ac
jjose21-source <j.jose21@escola.pr.gov.br>
2026-03-30 08:57:48 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Operações aritméticas
+
 // Soma
 
 let num1 = 2;
@@ -25,3 +27,65 @@ let divisao = 100 / 4;
 
 console.log("O quociente da divisão é:",divisao)
 
+// Operações de comparação
+
+// Igualdade
+
+console.log("10 é igual a 10?",10==10);
+
+// Diferença
+
+console.log("O nome Ana é diferente do nome Luiz?","Ana"!="Luiz");
+
+// Menor
+
+console.log("5 é menor que 10?",5<10);
+
+// Maior
+
+let valor = 10;
+
+console.log("O preço",valor,"é maior que 50?",valor>50);
+
+// Menor ou Igual
+
+valor = 20;
+
+console.log("O valor",valor,"é menor ou igual a 20?",valor<=20);
+
+// Maior ou Igual
+
+let idade = 17;
+
+console.log("Você é maior de idade?",idade>=18);
+
+// Operadores Ternários
+
+let nota = 7;
+
+let resultado = (nota >= 7) ? "Aprovado" : "Reprovado";
+
+idade = 18;
+
+let status = (idade >= 18) ? "Adulto" : "Menor";
+
+// Template Literals
+
+let nome = "Gabriel"
+
+console.log(`Olá, ${nome}`);
+
+let produto = "Amaciante";
+let preco = 19.99;
+let quantidade = 2;
+
+console.log(`Compra aprovada de ${quantidade} ${produto} com o preço unitário de ${preco}, valor total ${preco*quantidade}`);
+
+
+// Atividade de união
+
+nome = "Mateus"; 
+idade = 18;
+let mensagem = (idade >=18) ? `A pessoa ${nome} é maior de idade` : `A pessoa ${nome} é menor de idade`;
+
+console.log(mensagem);
